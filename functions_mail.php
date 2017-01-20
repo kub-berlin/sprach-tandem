b20a8bccfe2629ae053a9df998733c5d1325bba3
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-01-20 13:42:25 +0100

    refactor mail

--- functions_mail.php
+++ functions_mail.php
@@ -9,7 +9,6 @@ require_once 'Mail.php';
    ==================================*/
 function isValidEmail($email) {
 	return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email) && !(strpos($email, ",") > 0 OR strpos($email, ";") > 0 OR strpos($email, "\n") > 0) && !($email==="sayeemary@gmail.com");
-
 }
 
 
@@ -17,7 +16,13 @@ function sendEmail($to, $subject, $body, $replyto)
 {
 	$ret = 0;
 	$from = 'noreply@'.$GLOBALS['domain'];
-	$mail = mail($to , ($subject) , ($body), 'From: '.$GLOBALS["organisationName"].' <'.$from.">\n".'BCC: sprachtandem@kub-berlin.org'."\n".'Reply-To: '.$replyto."\nMIME-Version: 1.0\nContent-type: text/plain; charset=UTF-8\n");
+	$headers = array(
+		"From: {$GLOBALS["organisationName"]} <$from>",
+      "BCC: sprachtandem@kub-berlin.org",
+		"Reply-To: $replyto",
+		"MIME-Version: 1.0",
+		"Content-type: text/plain; charset=UTF-8");
+	$mail = mail($to, $subject, $body, implode("\n", $headers));
 	if (PEAR::isError($mail)) {
 		if ($GLOBALS['debug'] == 1)
 		{
@@ -36,7 +41,16 @@ function sendEmail($to, $subject, $body, $replyto)
 function send_notification_add($to, $name, $id, $hash, $label)
 {
 	$subject = sprintf($label["Add_email_subject"], $GLOBALS["organisationName"]);
-	$body = sprintf($label["Add_notification_email"], $name, $GLOBALS["organisationName"], $GLOBALS['tandem_root_path']."action=release&tid=".$id."&lang=".$label['lang']."&a=".$hash, $GLOBALS["organisationName"], $GLOBALS["organisationName"], $GLOBALS['tandem_root_path']."action=delete&tid=".$id."&lang=".$label['lang']."&a=".$hash, $GLOBALS['tandem_root_path']."action=edit&tid=".$id."&lang=".$label['lang']."&a=".$hash, $GLOBALS['email_orga']);
+	$baseLink = $GLOBALS['tandem_root_path'] . "tid=$id&lang={$label['lang']}&a=$hash";
+	$body = sprintf($label["Add_notification_email"],
+		$name,
+		$GLOBALS["organisationName"],
+		$baseLink . '&action=release',
+		$GLOBALS["organisationName"],
+		$GLOBALS["organisationName"],
+		$baseLink . '&action=delete',
+		$baseLink . '&action=edit',
+		$GLOBALS['email_orga']);
 
 	$gesendet = sendEmail($to, ($subject), ($body), 'noreply@'.$GLOBALS['domain']);
 	writeLog('send_notification_add: Email senden: '.$gesendet);
@@ -46,7 +60,17 @@ function send_notification_add($to, $name, $id, $hash, $label)
 function send_notification_view($to, $nameTo, $nameFrom, $spracheAng, $spracheGes, $alter, $geschlecht, $ort, $email, $beschreibung, $label)
 {
 	$subject = sprintf($label["View_email_subject"], $GLOBALS["organisationName"]);
-	$body = sprintf($label['View_notification_email'], $nameTo, $nameFrom, $label[$spracheAng], $label[$spracheGes], $alter, $geschlecht, $ort, $email, $beschreibung, $GLOBALS["organisationName"]);
+	$body = sprintf($label['View_notification_email'],
+		$nameTo,
+		$nameFrom,
+		$label[$spracheAng],
+		$label[$spracheGes],
+		$alter,
+		$geschlecht,
+		$ort,
+		$email,
+		$beschreibung,
+		$GLOBALS["organisationName"]);
 
 	$gesendet = sendEmail($to, ($subject), ($body), $email);
 	writeLog('send_notification_view: Email senden: '.$gesendet);
@@ -56,7 +80,13 @@ function send_notification_view($to, $nameTo, $nameFrom, $spracheAng, $spracheGe
 function send_notification_report($to, $name, $email, $text, $name_reported, $id_reported, $text_reported, $label)
 {
 	$subject = sprintf($label["Report_email_subject"], $GLOBALS["organisationName"]);
-	$body = sprintf($label["Report_email"], $name, $email, $text, $name_reported, $id_reported, $text_reported);
+	$body = sprintf($label["Report_email"],
+		$name,
+		$email,
+		$text,
+		$name_reported,
+		$id_reported,
+		$text_reported);
 
 	$gesendet = sendEmail($to, $subject, $body, $email);
 	writeLog('send_notification_report: Email senden: '.$gesendet);
@@ -66,7 +96,14 @@ function send_notification_report($to, $name, $email, $text, $name_reported, $id
 function send_reminder($to, $name, $id, $hash, $label)
 {
 	$subject = sprintf($label["Reminder_subject"], $GLOBALS["organisationName"]);
-	$body = sprintf($label["Reminder_email"], $name, $GLOBALS["organisationName"], $GLOBALS["organisationName"], $GLOBALS['tandem_root_path']."?action=delete&tid=".$id."&lang=".$label['lang']."&a=".$hash, $GLOBALS['tandem_root_path']."?action=edit&tid=".$id."&lang=".$label['lang']."&a=".$hash, $GLOBALS['email_orga']);
+	$baseLink = $GLOBALS['tandem_root_path'] . "?tid=$id&lang={$label['lang']}&a=$hash";
+	$body = sprintf($label["Reminder_email"],
+		$name,
+		$GLOBALS["organisationName"],
+		$GLOBALS["organisationName"],
+		$baseLink . "&action=delete",
+		$baseLink . "&action=edit",
+		$GLOBALS['email_orga']);
 
 	$gesendet = sendEmail($to, $subject, $body, 'noreply@'.$GLOBALS['domain']);
 	writeLog('send_notification_add: Email senden: '.$gesendet);
