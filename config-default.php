3fd33aaa7de18977038d971bb554f6a72e065b54
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-01-20 14:22:07 +0100

    remove no-op globals from config

--- config-default.php
+++ config-default.php
@@ -6,22 +6,11 @@
 //
 //#############################
 
-global $domain;
 $domain = 'example.com';
-
-global $tandem_root_path;
 $tandem_root_path = "https://$domain/root/path?";
-
-global $tandem_home_path;
 $tandem_home_path = "https://$domain/home/path?";
-
-global $logfile;
 $logfile = './tandem.log';
-
-global $errorMessage;
 $errorMessage = 'Sorry, something went wrong...';
-
-global $organisationName;
 $organisationName = 'Some Organisation';
 
 //##############################
@@ -30,10 +19,7 @@ $organisationName = 'Some Organisation';
 //
 //#############################
 
-global $debug;
 $debug = 1;
-
-global $showTitle;
 $showTitle = false;
 
 //##############################
@@ -43,23 +29,14 @@ $showTitle = false;
 //#############################
 
 // DATABASE CONNECTION
-global $mysql_server;
 $mysql_server = 'localhost';
-global $mysql_username;
 $mysql_username = 'username';
-global $mysql_password;
 $mysql_password = 'password';
 
-global $db_name;
 $db_name = 'sprachtandem';
 
 // TABLE DEFINITION
-global $db_table_name;
 $db_table_name = 'sprachtandem';
-
-global $db_colName_id, $db_colName_name, $db_colName_alter, $db_colName_ort, $db_colName_email, $db_colName_datum,
-$db_colName_geschlecht, $db_colName_spracheGes, $db_colName_spracheAng, $db_colName_skill, $db_colName_beschreibung, $db_colName_hash,
-$db_colName_lang, $db_colName_released, $db_colName_antworten;
 
 $db_colName_id = 'id';
 $db_colName_name = 'name';
@@ -77,7 +54,6 @@ $db_colName_lang = 'lang';
 $db_colName_released = 'released';
 $db_colName_antworten = 'antworten';
 
-global $db_columns;
 $db_columns = array(
 	$db_colName_id => null,
 	$db_colName_name => null,
@@ -95,7 +71,6 @@ $db_columns = array(
 	$db_colName_released => null,
 	$db_colName_antworten => null);
 
-global $table_page_size;
 $table_page_size = 20; //25;
 
 
@@ -105,13 +80,9 @@ $table_page_size = 20; //25;
 //
 //#############################
 
-global $email_from, $email_replyto, $email_orga;
-
 $email_from = 'noreply@example.com';
 $email_replyto = $email_from;
 $email_orga = 'sprachtandem@example.com';
-
-global $reminder_cyclic, $reminder_first;
 
 $reminder_cyclic = 1; //30;
 $reminder_first = 1; //3;
