811ce71b7b939d351fe9c9bb61f92ddee806ffbd
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-03-04 11:48:54 +0100

    refactor filter

--- partials/form_filter_lang.php
+++ partials/form_filter_lang.php
@@ -1,31 +1,30 @@
 <form action="<?php e($caller) ?>" method="GET">
-	<table class=filter_table>
-		<tr>
-			<th><?php e($label['Table_filtern']) ?>:</th>
-			<th>
-				<select name="filterAng">
-					<option><?php e($label['Table_filter_alle']) ?></option>
-					<?php foreach ($langs_ang as $key) : ?>
-						<option value="<?php e($key) ?>" <?php if ($filterAng == $key) : ?>selected<?php endif?>><?php e($label[$key]) ?></option>
-					<?php endforeach ?>
-				</select>
-			</th>
-			<th>
-				<select name="filterGes">
-					<option><?php e($label['Table_filter_alle']) ?></option>
-					<?php foreach ($langs_ges as $key) : ?>
-						<option value="<?php e($key) ?>" <?php if ($filterGes == $key) : ?>selected<?php endif?>><?php e($label[$key]) ?></option>
-					<?php endforeach ?>
-				</select>
-			</th>
-			<th>
-				<button type="submit">
-					<?php icon('funnel') ?>
-					<?php e($label['Table_filtern']) ?>
-				</button>
-			</th>
-		</tr>
-	</table>
+	<div class="filter-bar">
+		<?php e($label['Table_filtern']) ?>
+
+		<label title="<?php e($label['Table_col_spracheAng']) ?>">
+			<select name="filterAng">
+				<option><?php e($label['Table_filter_alle']) ?></option>
+				<?php foreach ($langs_ang as $key) : ?>
+					<option value="<?php e($key) ?>" <?php if ($filterAng == $key) : ?>selected<?php endif?>><?php e($label[$key]) ?></option>
+				<?php endforeach ?>
+			</select>
+		</label>
+
+		<label title="<?php e($label['Table_col_spracheGes']) ?>">
+			<select name="filterGes">
+				<option><?php e($label['Table_filter_alle']) ?></option>
+				<?php foreach ($langs_ges as $key) : ?>
+					<option value="<?php e($key) ?>" <?php if ($filterGes == $key) : ?>selected<?php endif?>><?php e($label[$key]) ?></option>
+				<?php endforeach ?>
+			</select>
+		</label>
+
+		<button type="submit">
+			<?php icon('funnel') ?>
+			<?php e($label['Table_filtern']) ?>
+		</button>
+	</div>
 
 	<input type="hidden" name="action" value="table" />
 	<input type="hidden" name="page" value="0" />
